3bfa7ec3f91eec7ab35dd16c3d126343c2934168
Nebojša Stričević <strika@hackberry.dev>
2019-08-03 20:13:36 +0200

    Implement World.create

--- spec/v1/WorldSpec.js
+++ spec/v1/WorldSpec.js
@@ -1,10 +1,11 @@
 describe("World", function() {
-  var World = require("../../lib/v1/World");
+  const World = require("../../lib/v1/World");
 
   describe("#create", function() {
     it("returns a new world", function() {
-      var world = World.create(3, 2);
-      expect(world).toBe(true);
+      const world = World.create(3, 2);
+      expect(world.size).toEqual(2);
+      expect(world.get(0).size).toEqual(3);
     });
   });
 });
