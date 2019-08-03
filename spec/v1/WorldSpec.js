c393766bd8db14c61da9c01f2f3898e7d49a9242
Nebojša Stričević <strika@hackberry.dev>
2019-08-03 21:10:01 +0200

    Implement World.nextState

--- spec/v1/WorldSpec.js
+++ spec/v1/WorldSpec.js
@@ -8,4 +8,20 @@ describe('World', () => {
       expect(world.get(0).size).toEqual(3);
     });
   });
+
+  describe('#nextState', () => {
+    it('applies the next state function to all cells', () => {
+      const nextCellState = (_, x, y) => x + y;
+      let world = World.create(2, 2);
+
+      world = World.nextState(world, nextCellState);
+
+      for (let i = 0; i < 2; i += 1) {
+        for (let j = 0; j < 2; j += 1) {
+          const cell = world.get(i).get(j);
+          expect(cell).toEqual(i + j);
+        }
+      }
+    });
+  });
 });
