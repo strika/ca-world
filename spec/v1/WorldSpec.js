6701be4c1848f1c204225e2c4ffa314e15edad0d
Nebojša Stričević <strika@hackberry.dev>
2019-08-03 20:17:57 +0200

    Turn off ESLint no-undef

--- spec/v1/WorldSpec.js
+++ spec/v1/WorldSpec.js
@@ -1,8 +1,8 @@
-describe("World", function() {
-  const World = require("../../lib/v1/World");
+const World = require('../../lib/v1/World');
 
-  describe("#create", function() {
-    it("returns a new world", function() {
+describe('World', () => {
+  describe('#create', () => {
+    it('returns a new world', () => {
       const world = World.create(3, 2);
       expect(world.size).toEqual(2);
       expect(world.get(0).size).toEqual(3);
