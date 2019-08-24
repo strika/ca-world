3323935fc31e6f50823842cc8926f353443823ca
Nebojša Stričević <strika@hackberry.dev>
2019-08-24 12:43:54 +0200

    Implement World.neighborhood

--- spec/v3/WorldSpec.js
+++ spec/v3/WorldSpec.js
@@ -25,13 +25,13 @@ describe('World', () => {
 
   describe('#neighborhood', () => {
     it('returns the neighborhood cells of the cell, including the cell', () => {
-      const world = Immutable.List(
+      const world = Immutable.List([
         Immutable.List([0, 1, 0, 0]),
         Immutable.List([1, 0, 1, 0]),
         Immutable.List([1, 0, 1, 0]),
         Immutable.List([1, 0, 1, 0]),
-      );
-      const neighborhood = World.neighborhood(world, 1, 1);
+      ]);
+      const neighborhood = World.neighborhood(world, 1, 1).toArray();
 
       expect(neighborhood).toEqual([0, 1, 0, 1, 0, 1, 1, 0, 1]);
     });
