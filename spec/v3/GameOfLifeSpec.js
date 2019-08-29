40a369e79add4f7adcccaf835086b089a5609cae
Nebojša Stričević <strika@hackberry.dev>
2019-08-29 17:42:49 +0200

    Implement skeleton for GameOfLife

--- spec/v3/GameOfLifeSpec.js
+++ spec/v3/GameOfLifeSpec.js
@@ -1,2 +1,29 @@
 describe('GameOfLife', () => {
+  describe('#nextCellState', () => {
+    describe('when cell is alive', () => {
+      describe('when cell has fewer than two live neighbors', () => {
+        it('dies', () => {
+          const world = Immutable.List([
+            Immutable.List([0, 0, 0]),
+            Immutable.List([1, 1, 0]),
+            Immutable.List([0, 0, 0]),
+          ]);
+
+          expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(0);
+        });
+      });
+
+      describe('when cell has 2 live neighbors', () => {
+        it('stays alive', () => {
+          const world = Immutable.List([
+            Immutable.List([1, 0, 0]),
+            Immutable.List([1, 1, 0]),
+            Immutable.List([0, 0, 0]),
+          ]);
+
+          expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(1);
+        });
+      });
+    });
+  });
 });
