05a3b969332aee499c598351bfd4d695245b9ed1
Nebojša Stričević <strika@hackberry.dev>
2019-09-08 18:30:44 +0200

    Add spec for more than 3 live neighbors

--- spec/v3/GameOfLifeSpec.js
+++ spec/v3/GameOfLifeSpec.js
@@ -36,6 +36,18 @@ describe('GameOfLife', () => {
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(1);
         });
       });
+
+      describe('when cell has more than 3 live neighbors', () => {
+        it('dies', () => {
+          const world = Immutable.List([
+            Immutable.List([1, 1, 0]),
+            Immutable.List([1, 1, 0]),
+            Immutable.List([0, 0, 1]),
+          ]);
+
+          expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(0);
+        });
+      });
     });
   });
 });
