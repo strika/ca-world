220cbfb0ccb6b591e351352e4f05b6d654fe9a9e
Nebojša Stričević <strika@hackberry.dev>
2019-09-08 18:42:22 +0200

    Implement rule for dead cell with 3 live neighbors

--- spec/v3/GameOfLifeSpec.js
+++ spec/v3/GameOfLifeSpec.js
@@ -62,6 +62,18 @@ describe('GameOfLife', () => {
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(0);
         });
       });
+
+      describe('when cell has 3 live neighbors', () => {
+        it('becomes alive', () => {
+          const world = Immutable.List([
+            Immutable.List([1, 1, 0]),
+            Immutable.List([0, 0, 1]),
+            Immutable.List([0, 0, 0]),
+          ]);
+
+          expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(1);
+        });
+      });
     });
   });
 });
