a33f7584bb5b5fe240963675f474f10ea586d090
Nebojša Stričević <strika@hackberry.dev>
2019-11-04 17:17:33 +0100

    Don't use Immutable in World.create

--- spec/v4/WorldSpec.js
+++ spec/v4/WorldSpec.js
@@ -2,8 +2,8 @@ describe('World', () => {
   describe('#create', () => {
     it('returns a new world', () => {
       const world = World.create(3, 2);
-      expect(world.size).toEqual(2);
-      expect(world.get(0).size).toEqual(3);
+      expect(world.length).toEqual(2);
+      expect(world[0].length).toEqual(3);
     });
   });
 
