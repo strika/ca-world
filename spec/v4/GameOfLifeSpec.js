2e99e3cb9e4b28c6f79b65f12346e909022eb1f3
Nebojša Stričević <strika@hackberry.dev>
2019-11-04 17:33:34 +0100

    Don't use Immutable in GameOfLifeSpec

--- spec/v4/GameOfLifeSpec.js
+++ spec/v4/GameOfLifeSpec.js
@@ -3,11 +3,11 @@ describe('GameOfLife', () => {
     describe('when cell is alive', () => {
       describe('when cell has fewer than two live neighbors', () => {
         it('dies', () => {
-          const world = Immutable.List([
-            Immutable.List([0, 0, 0]),
-            Immutable.List([1, 1, 0]),
-            Immutable.List([0, 0, 0]),
-          ]);
+          const world = [
+            [0, 0, 0],
+            [1, 1, 0],
+            [0, 0, 0],
+          ];
 
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(0);
         });
@@ -15,11 +15,11 @@ describe('GameOfLife', () => {
 
       describe('when cell has 2 live neighbors', () => {
         it('stays alive', () => {
-          const world = Immutable.List([
-            Immutable.List([1, 0, 0]),
-            Immutable.List([1, 1, 0]),
-            Immutable.List([0, 0, 0]),
-          ]);
+          const world = [
+            [1, 0, 0],
+            [1, 1, 0],
+            [0, 0, 0],
+          ];
 
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(1);
         });
@@ -27,11 +27,11 @@ describe('GameOfLife', () => {
 
       describe('when cell has 3 live neighbors', () => {
         it('stays alive', () => {
-          const world = Immutable.List([
-            Immutable.List([1, 1, 0]),
-            Immutable.List([1, 1, 0]),
-            Immutable.List([0, 0, 0]),
-          ]);
+          const world = [
+            [1, 1, 0],
+            [1, 1, 0],
+            [0, 0, 0],
+          ];
 
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(1);
         });
@@ -39,11 +39,11 @@ describe('GameOfLife', () => {
 
       describe('when cell has more than 3 live neighbors', () => {
         it('dies', () => {
-          const world = Immutable.List([
-            Immutable.List([1, 1, 0]),
-            Immutable.List([1, 1, 0]),
-            Immutable.List([0, 0, 1]),
-          ]);
+          const world = [
+            [1, 1, 0],
+            [1, 1, 0],
+            [0, 0, 1],
+          ];
 
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(0);
         });
@@ -53,11 +53,11 @@ describe('GameOfLife', () => {
     describe('when cell is dead', () => {
       describe('when cell has 2 live neighbors', () => {
         it('stays dead', () => {
-          const world = Immutable.List([
-            Immutable.List([1, 1, 0]),
-            Immutable.List([0, 0, 0]),
-            Immutable.List([0, 0, 0]),
-          ]);
+          const world = [
+            [1, 1, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+          ];
 
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(0);
         });
@@ -65,11 +65,11 @@ describe('GameOfLife', () => {
 
       describe('when cell has 3 live neighbors', () => {
         it('becomes alive', () => {
-          const world = Immutable.List([
-            Immutable.List([1, 1, 0]),
-            Immutable.List([0, 0, 1]),
-            Immutable.List([0, 0, 0]),
-          ]);
+          const world = [
+            [1, 1, 0],
+            [0, 0, 1],
+            [0, 0, 0],
+          ];
 
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(1);
         });
@@ -77,11 +77,11 @@ describe('GameOfLife', () => {
 
       describe('when cell has more than 3 live neighbors', () => {
         it('stays dead', () => {
-          const world = Immutable.List([
-            Immutable.List([1, 1, 0]),
-            Immutable.List([0, 0, 0]),
-            Immutable.List([1, 0, 1]),
-          ]);
+          const world = [
+            [1, 1, 0],
+            [0, 0, 0],
+            [1, 0, 1],
+          ];
 
           expect(GameOfLife.nextCellState(world, 1, 1)).toEqual(0);
         });
